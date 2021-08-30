57f5357cd312a2785287d1bb8fa3eaef809ed1ad
Mark Pieciak <mpieciak18@msn.com>
2021-08-30 17:00:35 -0400

    render player grids

---
+++ main.js
@@ -0,0 +1,5 @@
+import {initDisplay} from './scripts/display.js'
+
+const display = initDisplay()
+const gridOne = display.initGrid('grid-one', 1)
+const gridTwo = display.initGrid('grid-two', 2)
--- scripts/display.js
+++ scripts/display.js
@@ -1,30 +1,30 @@
-const display = {
-    gridOne: initGrid('grid-one', 1),
-    gridTwo: initGrid('grid-two', 2),
+const initDisplay = () => ({
     initGrid(gridId, gridNum) {
         const grid = document.getElementById(gridId)
         for (let i = 0; i < 10; i++) {
-            const row = initGridRow(gridNum, i)
+            const row = this.initGridRow(gridNum, i)
             grid.appendChild(row)
         }
+        console.log(grid)
+        return grid
     },
     initGridRow(gridNum, rowNum) {
         const row = document.createElement('div')
         row.classList = 'grid-row'
         row.id = `${gridNum}: ${rowNum}`
         for (let i = 1; i < 11; i++) {
-            const box = initGridBox(gridNum, rowNum, i)
+            const box = this.initGridBox(gridNum, rowNum, i)
             row.appendChild(box)
         }
         return row
     },
-    initGridBox(gridNum, rowNum, boxNum) {
+    initGridBox(gridNum, rowNum, num) {
         const box = document.createElement('div')
         box.classList = 'grid-box'
-        const boxNum = rowNum * 10 + boxNum
+        const boxNum = rowNum * 10 + num
         box.id = `${gridNum}: ${boxNum}`
         return box
     }
-}
+})
 
-export {display}
+export {initDisplay}
